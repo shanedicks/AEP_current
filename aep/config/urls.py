65e5d837f82161fd0537571626538cd17a46610f
Shane Dicks <shane.dicks1@gmail.com>
2016-08-07 17:50:03 -0500

    First commit of my third attempt at a DCCAEP web application

---
+++ aep/config/urls.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.views import defaults as default_views
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+
+    # Django Admin Stuff
+    url(r'^admin/', admin.site.urls),
+
+    # Other Stuff
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception('BadRequest!')}),
+        url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
+        url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page Not Found')}),
+        url(r'^500/$', default_views.server_error),
+    ]
