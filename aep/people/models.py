3cbfbae3e965808c0f19572a8a9c6cfc26230683
Shane Dicks <shane.dicks1@gmail.com>
2016-08-07 21:21:10 -0500

    Started people app

---
+++ aep/people/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+from django.utils.crypto import get_random_string
+
+
+class Profile(models.Model):
+
+    phone = models.CharField(max_length=20, blank=True)
+    alt_phone = models.CharField(max_length=20, blank=True)
+    street_address_1 = models.CharField(max_length=60, blank=True)
+    street_address_2 = models.CharField(max_length=60, blank=True)
+    city = models.CharField(max_length=30, blank=True)
+    state = models.CharField(max_length=2, blank=True)
+    dob = models.DateField(null=True, blank=True)
+    emergency_contact = models.CharField(max_length=60, blank=True)
+    ec_phone = models.CharField(max_length=20, blank=True)
+    ec_email = models.EmailField(max_length=40, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+
+def make_AEP_ID():
+    return get_random_string(length=8, allowed_chars='0123456789')
+
+
+class Student(Profile):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_("user"))
+
+    intake_date = models.DateField(null=True, blank=True)
+    WRU_ID = models.IntegerField(null=True, blank=True)
+    AEP_ID = models.IntegerField(unique=True, default=make_AEP_ID)
+
+
+class Staff(Profile):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_("user"))
+
+    bio = models.TextField(blank=True)
