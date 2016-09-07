ba593b7f3bf0b78f8200d2a2008167ca397724db
Shane Dicks <shane.dicks1@gmail.com>
2016-09-06 21:09:58 -0500

    people admin edits

--- aep/people/admin.py
+++ aep/people/admin.py
@@ -16,7 +16,22 @@ class StudentAdmin(admin.ModelAdmin):
 
     list_display = ("__str__", "AEP_ID", "WRU_ID", "dob")
 
-    search_fields = ["user__first_name", "user__last_name"]
+    search_fields = ["user__first_name", "user__last_name", 'WRU_ID']
+
+    fields = [
+        "user",
+        ("WRU_ID",
+         "AEP_ID"),
+        "slug",
+        "intake_date",
+        "dob",
+        ("phone",
+         "alt_phone"),
+        ("street_address_1",
+         "street_address_2"),
+        "city",
+        "state",
+    ]
 
 
 admin.site.register(Student, StudentAdmin)
