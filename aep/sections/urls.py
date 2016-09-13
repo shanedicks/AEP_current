4f41f1202504eefef2eba0d514bff1cd34e76aac
Shane Dicks <shane.dicks1@gmail.com>
2016-09-12 22:03:45 -0500

    student_enrollment

--- aep/sections/urls.py
+++ aep/sections/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^$', views.ClassListView.as_view(),
+        name='class list'),
     url(r'^(?P<slug>[a-zA-Z0-9]{5})/$',
         views.ClassDetailView.as_view(),
         name='class detail'),
-    url(r'^$', views.ClassListView.as_view(),
-        name='class list'),
+    url(r'^(?P<slug>[a-zA-Z0-9]{5})/enroll-student/$',
+        views.EnrollStudentView.as_view(),
+        name='enroll student'),
 ]
--- aep/sections/views.py
+++ aep/sections/views.py
@@ -1,6 +1,6 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Section
+from .models import Section, Enrollment
 
 
 class ClassListView(LoginRequiredMixin, ListView):
@@ -13,3 +13,10 @@ class ClassDetailView(LoginRequiredMixin, DetailView):
 
     model = Section
     template_name = 'sections/class_detail.html'
+
+
+class EnrollStudentView(LoginRequiredMixin, CreateView):
+
+    model = Enrollment
+    template_name = 'sections/enroll_student.html'
+    fields = ['student', 'status']
