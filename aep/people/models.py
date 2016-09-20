41b645a5cf1927f8604705bb14851c60313a4858
Shane Dicks <shane.dicks1@gmail.com>
2016-09-19 20:46:00 -0500

    WIOA model work

--- aep/people/models.py
+++ aep/people/models.py
@@ -316,6 +316,16 @@ class Student(Profile):
         default='S',
         verbose_name=_('Marital Status')
     )
+    native_language = models.CharField(
+        max_length=20,
+        blank=True,
+        verbose_name=_("Native Language")
+    )
+    country = models.CharField(
+        max_length=20,
+        blank=True,
+        verbose_name=_("Country of Highest Education")
+    )
 
     def has_WRU_ID(self):
         pass
@@ -358,8 +368,265 @@ class Staff(Profile):
 
 class WIOA(models.Model):
 
+    EMPLOYMENT_STATUS_CHOICES = (
+        ("1", "Employed"),
+        ("2", "Unemployed"),
+        ("3", "Unemployed - Not looking for work"),
+        ("4", "Not in labor force"),
+        ("5", "Employed, but recieved notice of termination or Military seperation is pending"),
+        ("6", "Not in labor force and/or not looking for work"),
+    )
+    MIGRANT_SEASONAL_STATUS_CHOICES = (
+        ("1", "Seasonal Farmworker"),
+        ("2", "Migrant and Seasonal Farmworker"),
+        ("3", "A Dependant of a Seasonal, or Migrant and Seasonal Farmworker"),
+        ("4", "No"),
+    )
+    YES_NO_UNKNOWN = (
+        ("1", "Yes"),
+        ("2", "No"),
+        ("3", "Unknown"),
+    )
+    YES_NO = (
+        ("", "Please Select"),
+        ("1", "Yes"),
+        ("2", "No"),
+    )
+    ONE_STOP_CHOICES = (
+        ("1", "Yes, Local Formula"),
+        ("2", "Yes, Statewide"),
+        ("3", "Yes, Both Local and Statewide"),
+    )
+    VOCATIONAL_REHAB_CHOICES = (
+        ("", "Please Select"),
+        ("1", "Vocational Rehabilitation"),
+        ("2", "Vocational Rehabilitation and Employment, Statewide"),
+        ("3", "Both, VR and VR&E"),
+        ("4", "No"),
+    )
+    SCHOOL_STATUS_CHOICES = (
+        ("", "Please Select"),
+        ("1", "In-School, H.S. or less"),
+        ("2", "In-School, Alternative School"),
+        ("3", "In-School, Post H.S."),
+        ("4", "Not attending school or H.S. dropout"),
+        ("5", "Not attending school; H.S. graduate"),
+        ("6", "Not attending school; within age of compulsory school attendance"),
+    )
+    TRAINING_TYPE_CHOICES = (
+        ("1", "On the Job Training"),
+        ("2", "Skill Upgrading"),
+        ("3", "Entrepreneurial Training (non-WIOA Youth)"),
+        ("4", "ABE or ESL in conjunction with Training (non-TAA funded)"),
+        ("5", "Customized Training"),
+        ("6", "Other Occupational Skills Training"),
+        ("7", "Remedial Training (ABE/ESL – TAA only)"),
+        ("8", "Prerequisite Training"),
+        ("9", "Registered Apprenticeship"),
+        ("10", "Youth Occupational Skills Training"),
+        ("11", "Other Non-Occupational-Skills Training"),
+        ("0", "No Training Service"),
+    )
+    PROGRAM_OF_STUDY_CHOICES = (
+        ("1", "A program of study leading to an industry-recognized certificate or certification"),
+        ("2", "A program of study leading to a certificate of completion of an apprenticeship"),
+        ("3", "A program of study leading to a license recognized by the State involved or the Federal Government"),
+        ("4", "A program of study leading to an associate degree"),
+        ("5", "A program of study leading to a baccalaureate degree"),
+        ("6", "A program of study leading to a community college certificate of completion"),
+        ("7", "A program of study leading to a secondary school diploma or its equivalent"),
+        ("8", "A program of study leading to employment"),
+        ("9", "A program of study leading to a measureable skills gain leading to a credentiacredential or employmentment"),
+        ("10", "Youth Occupational Skills Training"),
+    )
+    HIGHEST_COMPLETED_CHOICES = (
+        ("1", "NO FORMAL SCHOOL"),
+        ("2", "COMPLETED 1 YEAR"),
+        ("3", "COMPLETED 2 YEARS"),
+        ("4", "COMPLETED 3 YEARS"),
+        ("5", "COMPLETED 4 YEARS"),
+        ("6", "COMPLETED 5 YEARS"),
+        ("7", "COMPLETED 6 YEARS"),
+        ("8", "COMPLETED 7 YEARS"),
+        ("9", "COMPLETED 8 YEARS"),
+        ("10", "COMPLETED 9 YEARS"),
+        ("11", "COMPLETED 10 YEARS"),
+        ("12", "COMPLETED 11 YEARS"),
+        ("13", "COMPLETED 12 YEARS (HS DIPLOMA NOT EARNED)"),
+        ("14", "COMPLETED 13 YEARS"),
+        ("15", "COMP 14 YEARS/ASSC DEG/TECHNICAL DIPLOMA"),
+        ("16", "COMPLETED 15 YEARS"),
+        ("17", "COMPLETED BACHELOR DEGREE"),
+        ("18", "COMPLETED BEYOND BACHELOR DEGREE"),
+        ("19", "HIGH SCHOOL EQUIVALENCY"),
+        ("20", "CERTIFICATE OF ATTENDANCE OR COMPLETION (HS ONLY)"),
+        ("21", "POST SECONDARY DEGREE/CERTIFICATE EARNED"),
+        ("22", "COMPLETED 12 YEARS (HS DIPLOMA EARNED)"),
+    )
+    COMPLETED_AT_ENTRY_CHOICES = (
+        ("1", "NO SCHOOL GRADE COMPLETED"),
+        ("2", "COMPLETED 1 YEAR"),
+        ("3", "COMPLETED 2 YEARS"),
+        ("4", "COMPLETED 3 YEARS"),
+        ("5", "COMPLETED 4 YEARS"),
+        ("6", "COMPLETED 5 YEARS"),
+        ("7", "COMPLETED 6 YEARS"),
+        ("8", "COMPLETED 7 YEARS"),
+        ("9", "COMPLETED 8 YEARS"),
+        ("10", "COMPLETED 9 YEARS"),
+        ("11", "COMPLETED 10 YEARS"),
+        ("12", "COMPLETED 11 YEARS"),
+        ("13", "COMPLETED 12 YEARS"),
+    )
+    SCHOOL_LOCATION_CHOICES = (
+        ("", "Please Select"),
+        ("1", "US Based"),
+        ("2", "Non-US Based"),
+    )
+
     student = models.OneToOneField(
         Student,
         models.CASCADE,
         related_name='WIOA'
     )
+    current_employment_status = models.CharField(
+        max_length=2,
+        choices=EMPLOYMENT_STATUS_CHOICES,
+        default="2",
+        verbose_name=_("Current Employment Status")
+    )
+    employer = models.CharField(
+        max_length=25,
+        blank=True,
+        verbose_name=_("Employer")
+    )
+    occupation = models.CharField(
+        max_length=25,
+        blank=True,
+        verbose_name=_("Employer")
+    )
+    migrant_seasonal_status = models.CharField(
+        max_length=2,
+        choices=MIGRANT_SEASONAL_STATUS_CHOICES,
+        default="4",
+        verbose_name=_("Migrant and Seasonal Farmworker Status")
+    )
+    long_term_unemployed = models.BooleanField(
+        default=False,
+        verbose_name=_("Long-term Unemployed"),
+        help_text=_("More than 27 consecutive weeks")
+    )
+    single_parent = models.BooleanField(
+        default=False,
+        verbose_name=_("Single Parent")
+    )
+    public_assistance = models.BooleanField(
+        default=False,
+        verbose_name=_("Public Assistance")
+    )
+    rural_area = models.BooleanField(
+        default=False,
+        verbose_name=_("Rural Area")
+    )
+    displaced_homemaker = models.BooleanField(
+        default=False,
+        verbose_name=_("Displaced Homemaker")
+    )
+    dislocated_worker = models.BooleanField(
+        default=False,
+        verbose_name=_("Dislocated Worker")
+    )
+    cult_barriers_hind_emp = models.BooleanField(
+        default=False,
+        verbose_name=_("Cultural Barriers Hindering Employment")
+    )
+    in_foster_care = models.BooleanField(
+        default=False,
+        verbose_name=_("In Foster Care")
+    )
+    aged_out_foster_care = models.BooleanField(
+        default=False,
+        verbose_name=_("Aged out of Foster Care")
+    )
+    exhaust_tanf = models.BooleanField(
+        default=False,
+        verbose_name=_("Exhausting TANF Within 2 Years")
+    )
+    disability_status = models.CharField(
+        max_length=1,
+        choices=YES_NO_UNKNOWN,
+        blank=True
+    )
+    job_corps = models.CharField(
+        max_length=1,
+        choices=YES_NO_UNKNOWN,
+        blank=True
+    )
+    youth_build = models.CharField(
+        max_length=1,
+        choices=YES_NO_UNKNOWN,
+        blank=True
+    )
+    low_income = models.BooleanField(
+        default=False,
+        verbose_name=_("Low Income")
+    )
+    low_literacy = models.BooleanField(
+        default=False,
+        verbose_name=_("Low Literacy")
+    )
+    # Tick Checkboxes
+    # A
+    recieves_public_assistance = models.BooleanField(
+        default=False,
+    )
+    # B
+    low_family_income = models.BooleanField(
+        default=False,
+    )
+    # C
+    free_lunch_youth = models.BooleanField(
+        default=False,
+    )
+    # D
+    state_payed_foster = models.BooleanField(
+        default=False,
+    )
+    # E
+    disabled_in_poverty = models.BooleanField(
+        default=False,
+    )
+    # F
+    homeless = models.BooleanField(
+        default=False,
+    )
+    # G
+    youth_in_high_poverty_area = models.BooleanField(
+        default=False,
+    )
+    # Ex Offender
+    # A
+    subject_of_criminal_justice = models.BooleanField(
+        default=False,
+    )
+    # B
+    arrest_record_employment_barrier = models.BooleanField(
+        default=False,
+    )
+    # Homeless Individual
+    # A
+    lacks_adequate_reisdence = models.BooleanField(
+        default=False,
+    )
+    # B
+    irregular_sleep_accomodation = models.BooleanField(
+        default=False,
+    )
+    # C
+    migratory_child = models.BooleanField(
+        default=False,
+    )
+    # D
+    runaway_youth = models.BooleanField(
+        default=False,
+        )
