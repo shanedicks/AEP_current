35a6b02fde099042b9bea54d1d890b0ac8fb2a02
shane <shane.dicks1@gmail.com>
2022-10-11 19:35:19 -0700

    fixed for heroku redis upgrade

--- aep/config/celery.py
+++ aep/config/celery.py
@@ -5,15 +5,17 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-app = Celery('config')
+app = Celery('config',
+	redis_background_use_ssl = {
+	'ssl_cert_reqs': ssl.CERT_NONE
+	}
+)
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-redis_background_use_ssl = {
-	'ssl_cert_reqs': ssl.CERT_NONE
-}
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
