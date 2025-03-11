46ac408ad98ca9be362c02e2d3d6dd4db50aeb93
Shane Dicks <shane.dicks@gmail.com>
2025-03-11 14:21:43 -0400

    added forms 13 and 14 to TABE model

--- aep/assessments/models.py
+++ aep/assessments/models.py
@@ -450,11 +450,15 @@ class Tabe(NRSTest):
     TEN = '10'
     ELEVEN = '11'
     TWELVE = '12'
+    THIRTEEN = '13'
+    FOURTEEN = '14'
     FORM_CHOICES = (
         (NINE, '9'),
         (TEN, '10'),
         (ELEVEN, '11'),
-        (TWELVE, '12')
+        (TWELVE, '12'),
+        (THIRTEEN,'13'),
+        (FOURTEEN, '14')
     )
 
     A = "A"
