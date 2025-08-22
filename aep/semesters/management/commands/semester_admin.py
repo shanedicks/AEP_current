5499ef4299fd01caa2f9b525bf0d6a447fba415b
Shane Dicks <shane.dicks@gmail.com>
2025-08-22 13:46:56 -0400

    extending early session semester_admin tasks to 17 days

--- aep/semesters/management/commands/semester_admin.py
+++ aep/semesters/management/commands/semester_admin.py
@@ -1,10 +1,13 @@
 import time
+import logging
 from datetime import timedelta
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from sections.tasks import create_classroom_section_task, add_TA_task
 from semesters.models import Semester
 from semesters.tasks import create_missing_g_suite_task, first_class_warning_report_task
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -15,13 +18,14 @@ class Command(BaseCommand):
 			start_date__lte=now + timedelta(days=2),
 			end_date__gte=now
 		)
+		logger.info(f"Beginning Semester Admin Process")
 		create_classroom_sections_group = Semester.objects.filter(start_date=now + timedelta(days=7))
 		for semester in create_classroom_sections_group:
 			section_ids = [s.id for s in semester.get_sections() if s.g_suite_id == '']
 			create_classroom_section_task.delay(section_ids)
 			time.sleep(300)
 			add_TA_task.delay(section_ids)
-		for semester in active.filter(start_date__gte=now - timedelta(days=14)):
+		for semester in active.filter(start_date__gte=now - timedelta(days=17)):
 			create_missing_g_suite_task.delay(semester.id)
 			time.sleep(10)
 			if now.weekday() in [1, 2, 3, 4]:
@@ -38,3 +42,4 @@ class Command(BaseCommand):
 		end_semester_group = Semester.objects.filter(end_date=now - timedelta(days=14))
 		for semester in end_semester_group:
 			semester.end()
+		logger.info(f"Semester Admin Process Complete")
